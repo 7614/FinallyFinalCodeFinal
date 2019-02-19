5321ffcdb1c3bcc048a4288515aa969457c588bc
TurtlyP <45469554+TurtlyP@users.noreply.github.com>
2019-02-19 15:53:29 -0500

    Merged MoveFlap

--- 2019UpdatedFinalCode/src/main/java/frc/robot/commands/MoveFlap.java
+++ 2019UpdatedFinalCode/src/main/java/frc/robot/commands/MoveFlap.java
@@ -57,9 +57,8 @@ public class MoveFlap extends Command {
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-
-    return true;
-  }
+  return true;
+ }
 
   // Called once after isFinished returns true
   @Override
